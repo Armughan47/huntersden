3df6213af7ef93669a9a3b0adcf1ec0ce2a13d0c
Armughan47 <45979404+Armughan47@users.noreply.github.com>
2018-12-19 01:11:50 +0500

    Add files via upload

---
+++ sp16-bcs-036.m
@@ -0,0 +1,55 @@
+﻿
+
+rad=6;
+y=rad;
+x=0;
+
+d=1-rad;
+
+hold on
+grid on
+
+axis([-10 10 -10 10])
+
+scatter (x,y,60,"filled")
+scatter (y,x,60,"filled")
+
+scatter (y,-x,60,"filled")
+
+scatter (-y,-x,60,"filled")
+
+scatter (-x,-y,60,"filled")
+
+scatter (-x,y,60,"filled")
+
+scatter (x,-y,60,"filled")
+
+
+
+scatter (-y,x,60,"filled")
+
+
+while (y>x)
+    if(d<0)
+        d+= x*2+3;
+        
+        x++;
+        
+        else
+            
+        d+=(x-y)*2+5;
+        
+        x++;
+        
+        y--;
+    end
+scatter (x,y,60,"filled")
+scatter (-x,y,60,"filled")
+scatter (x,-y,60,"filled")
+scatter (-x,-y,60,"filled")
+scatter (y,x,60,"filled")
+scatter (-y,x,60,"filled")
+scatter (y,-x,60,"filled")
+scatter (-y,-x,60,"filled")
+end
+    
